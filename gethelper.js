34aac60b1e7e1e0318061209dde7883a0ef46128
danjamf <dan.cuddeford@jamf.com>
2024-05-17 14:27:17 -0700

    first commit

---
+++ gethelper.js
@@ -0,0 +1,33 @@
+function radarechotest(request, session, config) {
+  log("Virtual Test running")
+  requestpath = request.URL.split('/radar')[1]
+
+
+  hold = request.Headers["Authorization"][0]
+  testholdencoded = hold.split(' ')[1]
+  testholddecoded = b64dec(testholdencoded)
+  xsrf = testholddecoded.split('&')[0]
+  sessioncookie = testholddecoded.split('&')[1]
+
+  newRequest = {
+    "Method": "GET",
+    "Body": "",
+    "Domain": "https://radar.wandera.com",
+    "Headers": {"Accept": "application/json", "X-Xsrf-Token": xsrf, "Cookie": "XSRF-TOKEN="+xsrf+";SESSION="+sessioncookie}
+    "Resource": requestpath,
+    "FormData": {}
+  };
+  
+  response = TykMakeHttpRequest(JSON.stringify(newRequest));
+  usableResponse = JSON.parse(response)
+
+  var responseObject = {
+    Body: usableResponse.Body
+    Headers: {
+      "Content-Type": "application/json"
+    },
+    Code: 200
+  }
+  
+  return TykJsResponse(responseObject, session.meta_data)   
+}
