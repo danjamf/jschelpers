51de09add1436c8f2ec0563665af0ffd00a57439
danjamf <dan.cuddeford@jamf.com>
2024-05-18 12:23:40 -0700

    echoing response status code

--- gethelper.js
+++ gethelper.js
@@ -1,33 +1,33 @@
 function radarechotest(request, session, config) {
-  log("Virtual Test running")
-  requestpath = request.URL.split('/radar')[1] //assumes path of helper is /radar - otherwise change it!
-
-
-  hold = request.Headers["Authorization"][0]
-  testholdencoded = hold.split(' ')[1]
-  testholddecoded = b64dec(testholdencoded)
-  xsrf = testholddecoded.split('&')[0]
-  sessioncookie = testholddecoded.split('&')[1]
-
-  newRequest = {
-    "Method": "GET",
-    "Body": "",
-    "Domain": "https://radar.wandera.com",
-    "Headers": {"Accept": "application/json", "X-Xsrf-Token": xsrf, "Cookie": "XSRF-TOKEN="+xsrf+";SESSION="+sessioncookie},
-    "Resource": requestpath,
-    "FormData": {}
-  };
+    log("Virtual Test running")
+    requestpath = request.URL.split('/radar')[1]
   
-  response = TykMakeHttpRequest(JSON.stringify(newRequest));
-  usableResponse = JSON.parse(response)
-
-  var responseObject = {
-    Body: usableResponse.Body,
-    Headers: {
-      "Content-Type": "application/json"
-    },
-    Code: 200 //need to change this to mirror the response code
+  
+    hold = request.Headers["Authorization"][0]
+    testholdencoded = hold.split(' ')[1]
+    testholddecoded = b64dec(testholdencoded)
+    xsrf = testholddecoded.split('&')[0]
+    sessioncookie = testholddecoded.split('&')[1]
+  
+    newRequest = {
+      "Method": "GET",
+      "Body": "",
+      "Domain": "https://radar.wandera.com",
+      "Headers": {"Accept": "application/json", "X-Xsrf-Token": xsrf, "Cookie": "XSRF-TOKEN="+xsrf+";SESSION="+sessioncookie},
+      "Resource": requestpath,
+      "FormData": {}
+    };
+    
+    response = TykMakeHttpRequest(JSON.stringify(newRequest));
+    usableResponse = JSON.parse(response)
+  
+    var responseObject = {
+      Body: usableResponse.Body,
+      Headers: {
+        "Content-Type": "application/json"
+      },
+      Code: usableResponse.Code
+    }
+    
+    return TykJsResponse(responseObject, session.meta_data)   
   }
-  
-  return TykJsResponse(responseObject, session.meta_data)   
-}
